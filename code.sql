0b6e56ab2499a3cfd14f3d5abe3dce148c4efe3b
emirhandlbr <113054383+emirhandilber@users.noreply.github.com>
2023-07-26 12:01:39 +0300

    Code

---
+++ code.sql
@@ -0,0 +1,9 @@
+
+--Lets sum the required column values
+SELECT SUM(CITY.POPULATION) FROM COUNTRY COUNTRY
+
+--Join the both  CITY and COUNTRY table in countrycode and code
+INNER JOIN CITY CITY ON CITY.COUNTRYCODE = COUNTRY.CODE
+
+--Lets constraint the result
+WHERE COUNTRY.CONTIENT = 'Asia'
